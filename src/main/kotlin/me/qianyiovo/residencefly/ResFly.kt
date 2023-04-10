fff356024079720192abfc868187befa3fc502ff
Qianyiovo <qianyiovo@outlook.com>
2023-04-10 14:07:40 +0800

    Optimization Code

--- src/main/kotlin/me/qianyiovo/residencefly/ResFly.kt
+++ src/main/kotlin/me/qianyiovo/residencefly/ResFly.kt
@@ -14,46 +14,54 @@ class ResFly(private val plugin: JavaPlugin) : CommandExecutor {
     override fun onCommand(commandSender: CommandSender, command: Command, string: String, args: Array<out String>): Boolean {
         if (commandSender !is Player) {
             commandSender.sendMessage("You can only use this command in the game")
-            return false
+            return true
         }
+
+        if (!commandSender.hasPermission("resfly.use")) {
+            commandSender.sendLocalizedErrorMessage()
+            return true
+        }
+
         val residencePlugin = Bukkit.getServer().pluginManager.getPlugin("Residence") as Residence?
         val residenceManager = residencePlugin?.residenceManager
-        val loc = (commandSender as Player?)?.location
+        val loc = commandSender.location
         val residence = residenceManager?.getByLoc(loc)
-        val locale = (commandSender as Player?)?.locale
-        if (!commandSender.hasPermission("resfly.use")) {
-            if (locale != null) {
-                return if (locale.startsWith("zh")) {
-                    commandSender.sendMessage("${ChatColor.RED}你没有权限访问此命令")
-                    false
-                } else {
-                    commandSender.sendMessage("${ChatColor.RED}You don't have permission to access this command")
-                    false
-                }
-            }
-        }
-        if (residence != null && residence.permissions.ownerUUID.equals(commandSender.uniqueId)) {
+
+        if (residence != null && residence.permissions.ownerUUID == commandSender.uniqueId) {
             commandSender.allowFlight = !commandSender.allowFlight
             commandSender.setMetadata("resfly.use", FixedMetadataValue(plugin, commandSender.allowFlight))
-            if (locale != null) {
-                return if (locale.startsWith("zh")) {
-                    commandSender.sendMessage(if (commandSender.allowFlight) "${ChatColor.GREEN}已启用飞行" else "${ChatColor.GREEN}已关闭飞行")
-                    true
-                } else {
-                    commandSender.sendMessage(if (commandSender.allowFlight) "${ChatColor.GREEN}Flight enabled" else "${ChatColor.GREEN}Flight disabled")
-                    true
-                }
-            }
+            commandSender.sendLocalizedFlightStatusMessage()
+            return true
+        } else {
+            commandSender.sendLocalizedNotInClaimMessage()
+            return true
         }
-        if (locale != null) {
-            return if (locale.startsWith("zh")) {
-                commandSender.sendMessage("${ChatColor.RED}你必须在你的领地中才能使用此指令")
-                false
-            } else {
-                commandSender.sendMessage("${ChatColor.RED}You must be in your claim to use this command")
-                false
-            }
+    }
+
+    private fun Player.sendLocalizedErrorMessage() {
+        val message = if (locale.startsWith("zh")) {
+            "${ChatColor.RED}你没有权限访问此命令"
+        } else {
+            "${ChatColor.RED}You don't have permission to access this command"
         }
-        return false
+        sendMessage(message)
+    }
+
+    private fun Player.sendLocalizedFlightStatusMessage() {
+        val message = if (locale.startsWith("zh")) {
+            if (allowFlight) "${ChatColor.GREEN}已启用飞行" else "${ChatColor.GREEN}已关闭飞行"
+        } else {
+            if (allowFlight) "${ChatColor.GREEN}Flight enabled" else "${ChatColor.GREEN}Flight disabled"
+        }
+        sendMessage(message)
+    }
+
+    private fun Player.sendLocalizedNotInClaimMessage() {
+        val message = if (locale.startsWith("zh")) {
+            "${ChatColor.RED}你必须在你的领地中才能使用此指令"
+        } else {
+            "${ChatColor.RED}You must be in your claim to use this command"
+        }
+        sendMessage(message)
     }
 }
